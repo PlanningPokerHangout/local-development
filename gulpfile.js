eeb59d57c9c57ad5099a0e9ead7f6e24e3ecf3d4
Tim Willis <tim.willis2@coxinc.com>
2015-12-14 17:44:09 -0600

    Remove more cruft

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,14 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var spawn = require('child_process').spawn;
-var copy = require('gulp-copy');
 
-
-gulp.task('webpack', function(cb) {
-    var cmd = spawn('node_modules/.bin/webpack', [], {stdio: 'inherit'});
-    cmd.on('close', function(code) {
-        console.log(code);
-        cb(code);
-    });
-});
 
 gulp.task('static', function() {
     gulp.src('static/**')
-        .pipe(copy('publish', {prefix: 1}));
+    .pipe(gulp.dest('publish'));
 });
 
-gulp.task('build', ['webpack', 'static']);
+gulp.task('build', ['static']);
 
 gulp.task('deploy', ['build'], function() {
     return gulp.src('./publish/**/*')
